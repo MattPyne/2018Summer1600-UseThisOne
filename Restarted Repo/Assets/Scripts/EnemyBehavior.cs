77312d49fbe08f8e8ffa1b903b8c7f9d61294bfd
Unknown <10820789@my.uvu.edu>
2018-06-09 01:27:40 -0600

    Added a white block gun and some targets

---
+++ Restarted Repo/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+
+	public int EnemyHealth;
+
+	private void Update()
+	{
+		Debug.Log(EnemyHealth.ToString());
+		if (EnemyHealth <= 0)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Restarted Repo/Assets/Scripts/GunBehavior.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEditor;
+using UnityEngine;
+
+public class GunBehavior : MonoBehaviour
+{
+
+	public Transform Barrel;
+	public float Range = 0f;
+
+	public float Delay = 0f;
+	bool Fired;
+
+	bool isAuto;
+	public KeyCode SwitchFireMode;
+	
+
+	private void Update()
+	{
+		if (Input.GetButton("Fire1") && !Fired && isAuto)
+		{
+			Fired = true;
+			StartCoroutine("FireAuto");
+		}
+
+		if (Input.GetButtonDown("Fire1") && !Fired && !isAuto)
+		{
+			Fired = true;
+			StartCoroutine("FireSemi");
+		}
+
+		if (Input.GetKeyDown(SwitchFireMode))
+		{
+			if (!isAuto)
+			{
+				isAuto = true;
+			}
+			else
+			{
+				isAuto = false;
+			}
+		}
+	}
+
+	IEnumerator FireAuto ()
+	{
+		RaycastHit hit;
+		Ray ray = new Ray(Barrel.position, transform.forward);
+
+		if (Physics.Raycast(ray, out hit, Range))
+		{
+			if(hit.collider.tag == "Enemy")
+			{
+				EnemyBehavior enemy = hit.collider.GetComponent<EnemyBehavior>();
+				enemy.EnemyHealth -= 1;
+			}
+		}
+
+		Debug.DrawRay(Barrel.position, transform.forward * Range, Color.green);
+		yield return new WaitForSeconds(Delay);
+		Fired = false;
+	}
+	IEnumerator FireSemi ()
+	{
+		RaycastHit hit;
+		Ray ray = new Ray(Barrel.position, transform.forward);
+
+		if (Physics.Raycast(ray, out hit, Range))
+		{
+			if(hit.collider.tag == "Enemy")
+			{
+				EnemyBehavior enemy = hit.collider.GetComponent<EnemyBehavior>();
+				enemy.EnemyHealth -= 1;
+			}
+		}
+
+		Debug.DrawRay(Barrel.position, transform.forward * Range, Color.red);
+		yield return null;
+		Fired = false;
+	}
+}
